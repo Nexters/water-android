98e0064b96ac91458422918404e727afbf5080cb
songmilee <lsm941010@gmail.com>
2020-07-25 15:06:30 +0900

    feature/mainRouting : 라우팅 함수 추가(#4)

--- app/src/main/java/appvian/water/buddy/view/MainActivity.kt
+++ app/src/main/java/appvian/water/buddy/view/MainActivity.kt
@@ -1,29 +1,59 @@
 package appvian.water.buddy.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import appvian.water.buddy.R
 import appvian.water.buddy.viewmodel.HomeViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.InternalCoroutinesApi
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var homeViewModel: HomeViewModel
 
+    private lateinit var fragmentList: List<Fragment>
 
     @InternalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
         homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         homeViewModel.getDaily().observe(this, androidx.lifecycle.Observer {
 
         })
 
-        supportFragmentManager.beginTransaction().replace(R.id.fragment,MainFragment()).commit()
+        setRouting()
     }
 
+    private fun setRouting() {
+        fragmentList = listOf(MainFragment()) //라우팅할 fragment 리스트
+
+        main_navigation.setOnNavigationItemSelectedListener { menu ->
+            when (menu.itemId) {
+                R.id.menu_home -> {
+                    replaceFragment(fragmentList[0])
+                }
+                R.id.menu_analytics -> {
+                }
+                R.id.menu_settings -> {
+                }
+            }
+
+            return@setOnNavigationItemSelectedListener true
+
+        }
+
+        supportFragmentManager.beginTransaction().add(R.id.fragment, fragmentList[0]).commit()
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment, fragment)
+            .commit();
+    }
 
 }
