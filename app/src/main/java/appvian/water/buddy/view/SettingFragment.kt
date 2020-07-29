ea88ecb763bb8489e9039383f9d5d5e6560adb03
Jeong-heon2 <cjhgg2@gmail.com>
2020-07-29 23:05:24 +0900

    SharedPreference LiveData 구현체 생성, 테스트 (#13)

--- app/src/main/java/appvian/water/buddy/view/SettingFragment.kt
+++ app/src/main/java/appvian/water/buddy/view/SettingFragment.kt
@@ -1,22 +1,52 @@
 package appvian.water.buddy.view
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import androidx.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import appvian.water.buddy.R
 import appvian.water.buddy.databinding.FragmentSettingBinding
+import appvian.water.buddy.viewmodel.SettingViewModel
+import kotlinx.android.synthetic.main.fragment_setting.*
 
 class SettingFragment : Fragment() {
     private lateinit var fragmentSettingBinding: FragmentSettingBinding
+    private lateinit var settingviewModel: SettingViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        settingviewModel = ViewModelProvider(this).get(SettingViewModel::class.java)
         fragmentSettingBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_setting, container, false)
+
+        //테스트 코드 . 초기에 목표량 설정할테니까 바로 get하면 될 듯
+        settingviewModel.setSharedPreferences("target_amount", "변경 전 ")
+        //참고 https://www.thetopsites.net/article/51790868.shtml
+        settingviewModel.getSharedPrefs()!!.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            txt_test.text = it
+            Log.d("TAG",it)
+            fragmentSettingBinding.executePendingBindings()
+        })
+
         return fragmentSettingBinding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //테스트 코드
+        btn_prefs_change.setOnClickListener {
+            settingviewModel.setSharedPreferences("target_amount", "변경 후 ")
+        }
+    }
+
+
 }
--- app/src/main/java/appvian/water/buddy/viewmodel/SettingViewModel.kt
+++ app/src/main/java/appvian/water/buddy/viewmodel/SettingViewModel.kt
@@ -1,7 +1,30 @@
 package appvian.water.buddy.viewmodel
 
 import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.lifecycle.AndroidViewModel
+import androidx.preference.PreferenceManager
+import appvian.water.buddy.model.data.SharedPreferenceBooleanLiveData
+import appvian.water.buddy.model.data.SharedPreferenceLiveData
+import appvian.water.buddy.model.data.SharedPreferenceStringLiveData
+import appvian.water.buddy.model.data.stringLiveData
+
 
 class SettingViewModel(application: Application) : AndroidViewModel(application) {
+
+    val target_amount_pref = application.getSharedPreferences("target_amount", Context.MODE_PRIVATE)
+
+    private var sharedPreferenceLiveData: SharedPreferenceStringLiveData? = null
+
+    fun getSharedPrefs(): SharedPreferenceStringLiveData? {
+        return sharedPreferenceLiveData
+    }
+
+    fun setSharedPreferences(key: String, value: String) {
+        val editor = target_amount_pref.edit()
+        editor.putString(key, value)
+        editor.apply()
+        sharedPreferenceLiveData = SharedPreferenceStringLiveData(target_amount_pref, key, value)
+    }
 }
