644c6eccd6dd15c47285ea6b8678f53b97c485d0
hyunss <khh3599@naver.com>
2020-08-04 22:57:34 +0900

    Intro second fragment layout 구현 완료

--- app/src/main/java/appvian/water/buddy/view/IntroFragment.kt
+++ app/src/main/java/appvian/water/buddy/view/IntroFragment.kt
@@ -1,19 +1,11 @@
 package appvian.water.buddy.view
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
 import appvian.water.buddy.R
-import appvian.water.buddy.databinding.ActivityIntroBinding
-import appvian.water.buddy.viewmodel.HomeViewModel
-import appvian.water.buddy.viewmodel.IntroFragmentViewModel
-import appvian.water.buddy.viewmodel.IntroViewModel
 import kotlinx.android.synthetic.main.intro_fragment.view.*
 
 class IntroFragment : Fragment() {
--- app/src/main/java/appvian/water/buddy/view/IntroSecondFragment.kt
+++ app/src/main/java/appvian/water/buddy/view/IntroSecondFragment.kt
@@ -1,14 +1,14 @@
 package appvian.water.buddy.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.ViewModelProvider
 import appvian.water.buddy.R
-import appvian.water.buddy.viewmodel.IntroViewModel
 import kotlinx.android.synthetic.main.intro_second_fragment.view.*
 
 class IntroSecondFragment: Fragment() {
@@ -20,7 +20,20 @@ class IntroSecondFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.intro_second_fragment, container, false)
+        val textcount = view.text_count
+        val edittext = view.name_edittext
+        edittext.addTextChangedListener(object:TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
 
+            @SuppressLint("SetTextI18n")
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val count: Int = edittext.text.toString().length
+                textcount.text= count.toString() + getString(R.string.intro_second_textcount)
+            }
+        })
         return view
     }
 
