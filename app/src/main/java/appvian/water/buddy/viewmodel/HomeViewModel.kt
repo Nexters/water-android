0c2c48be9b3875bdf52e757e6b013ccdfeb67267
Junho-Seo-0304 <55340588+Junho-Seo-0304@users.noreply.github.com>
2020-08-06 16:01:04 +0900

    HomeViewModel에서 주간 월간 데이터 삭제

--- app/src/main/java/appvian/water/buddy/viewmodel/HomeViewModel.kt
+++ app/src/main/java/appvian/water/buddy/viewmodel/HomeViewModel.kt
@@ -13,13 +13,9 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
 
     var dailyIntake: LiveData<List<Intake>>? = getDaily()
 
-    var weeklyIntake: LiveData<List<Intake>>? = getWeekly()
-
-    var monthlyIntake: LiveData<List<Intake>>? = getMonthly()
-
     var dailyAmount : LiveData<Int>? = getDailyDrinkedAmount()
 
-    val goalAmount = 2000
+    val requiredAmount = 2000
 
     var beforeChangeAmount = 0
 
@@ -31,16 +27,6 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
     private fun getDaily(): LiveData<List<Intake>>? {
         val dailyIntake = repository.getDaily(getToday(), getTomorrow())
         return dailyIntake
-    }
-
-    private fun getWeekly(): LiveData<List<Intake>>? {
-        val weeklyIntake = repository.getWeekly(getToday(), getWeekAgo())
-        return weeklyIntake
-    }
-
-    private fun getMonthly(): LiveData<List<Intake>>? {
-        val monthlyIntake = repository.getMonthly(getToday(), getMonthAgo())
-        return monthlyIntake
     }
 
     fun insert(intake: Intake) {
@@ -74,27 +60,5 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
         calendar.set(Calendar.MILLISECOND,0)
         val tomorrow = calendar.time.time
         return tomorrow
-    }
-
-    private fun getWeekAgo(): Long {
-        val calendar = Calendar.getInstance()
-        calendar.add(Calendar.DATE, -6)
-        calendar.set(Calendar.HOUR_OF_DAY,0)
-        calendar.set(Calendar.MINUTE,0)
-        calendar.set(Calendar.SECOND,0)
-        calendar.set(Calendar.MILLISECOND,0)
-        val aWeekAgo = calendar.time.time
-        return aWeekAgo
-    }
-
-    private fun getMonthAgo(): Long {
-        val calendar = Calendar.getInstance()
-        calendar.add(Calendar.MONTH, -1)
-        calendar.set(Calendar.HOUR_OF_DAY,0)
-        calendar.set(Calendar.MINUTE,0)
-        calendar.set(Calendar.SECOND,0)
-        calendar.set(Calendar.MILLISECOND,0)
-        val aMonthAgo = calendar.time.time
-        return aMonthAgo
     }
 }
