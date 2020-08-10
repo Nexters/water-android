1316d6ca1e8afbc3f3b1f150924b693cb52a645d
songmilee <lsm941010@gmail.com>
2020-08-10 12:44:47 +0900

    feature/weekly_chart : 일-토로 조회되게 변경

--- app/src/main/java/appvian/water/buddy/viewmodel/analytics/WeeklyViewModel.kt
+++ app/src/main/java/appvian/water/buddy/viewmodel/analytics/WeeklyViewModel.kt
@@ -10,21 +10,21 @@ import java.util.*
 
 class WeeklyViewModel(val repository: HomeRepository) {
     private val now = Calendar.getInstance()
-    var curWeek = now.get(Calendar.WEEK_OF_MONTH) - 1
+    var curWeek = now.get(Calendar.WEEK_OF_MONTH)
     val totalWeeks = now.getActualMaximum(Calendar.WEEK_OF_MONTH)
 
     private var weekDay: LiveData<List<Intake>>? = null
     val weekObserve: MediatorLiveData<List<BarEntry>> = MediatorLiveData()
-    val targetValue: Float = 1.6f
+    val targetValue: Float = 1.6f //Todo : sharedPreference에서 불러와야 함
 
     fun getWeekIntakeData() {
         val firstDate = TimeUtil.getCalendarInstance()
         firstDate.set(Calendar.WEEK_OF_MONTH, curWeek)
-        firstDate.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
+        firstDate.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY)
 
         val lastDate = TimeUtil.getCalendarInstance()
         lastDate.set(Calendar.WEEK_OF_MONTH, curWeek + 1)
-        lastDate.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
+        lastDate.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY)
 
         weekDay = repository.getWeeklyByDay(firstDate.timeInMillis, lastDate.timeInMillis)
         android.util.Log.d("week", "${firstDate.timeInMillis}, ${lastDate.timeInMillis}")
@@ -33,11 +33,12 @@ class WeeklyViewModel(val repository: HomeRepository) {
             weekObserve.addSource(wr, androidx.lifecycle.Observer { values ->
                 val barList = initBarList()
 
+                android.util.Log.d("weekly data", "$values")
                 val groupValue = values.groupBy { it.date }
                     .mapValues { Intake(it.key, 0, it.value.sumBy { it.amount }) }.values
 
                 for (i in groupValue) {
-                    val index = if (i.date.toInt() != 0) i.date.toInt() - 1 else 6
+                    val index = i.date.toInt()
                     barList[index] = BarEntry(i.amount / 1000f, index)
                 }
                 weekObserve.value = barList
@@ -47,8 +48,7 @@ class WeeklyViewModel(val repository: HomeRepository) {
 
     private fun initBarList(): ArrayList<BarEntry> = arrayListOf<BarEntry>().apply {
         for (j in 0..6) {
-            if (j == 0) add(BarEntry(0f, 6))
-            else add(BarEntry(0f, j - 1))
+            add(BarEntry(0f, j))
         }
     }
 }
