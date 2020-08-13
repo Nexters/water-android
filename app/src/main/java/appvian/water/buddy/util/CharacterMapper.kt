0b3d1d45c2b3817a5cf2f4b7a1f030d651610d87
songmilee <lsm941010@gmail.com>
2020-08-13 22:59:59 +0900

    feature/month_chart : 캐릭터 이미지 매퍼 추가

---
+++ app/src/main/java/appvian/water/buddy/util/CharacterMapper.kt
@@ -0,0 +1,12 @@
+package appvian.water.buddy.util
+
+import appvian.water.buddy.R
+
+object CharacterMapper {
+
+    private val characterIdList = arrayListOf(
+        R.mipmap.artboard_4_3_x, R.mipmap.artboard_5_3_x, R.mipmap.artboard_6_3_x,
+        R.mipmap.artboard_7_3_x, R.mipmap.artboard_8_3_x, R.mipmap.artboard_9_3_x,
+        R.mipmap.artboard_10_3_x, R.mipmap.artboard_11_3_x, R.mipmap.artboard_12_3_x
+    )
+}
