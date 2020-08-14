ddfe56c457ef2cf58ee16d3235a6e4de40677012
songmilee <lsm941010@gmail.com>
2020-08-14 20:32:44 +0900

    feature/month_chart : private 제거

--- app/src/main/java/appvian/water/buddy/util/CharacterMapper.kt
+++ app/src/main/java/appvian/water/buddy/util/CharacterMapper.kt
@@ -4,7 +4,7 @@ import appvian.water.buddy.R
 
 object CharacterMapper {
 
-    private val characterIdList = arrayListOf(
+    val characterIdList = arrayListOf(
         R.mipmap.artboard_4_3_x, R.mipmap.artboard_5_3_x, R.mipmap.artboard_6_3_x,
         R.mipmap.artboard_7_3_x, R.mipmap.artboard_8_3_x, R.mipmap.artboard_9_3_x,
         R.mipmap.artboard_10_3_x, R.mipmap.artboard_11_3_x, R.mipmap.artboard_12_3_x
