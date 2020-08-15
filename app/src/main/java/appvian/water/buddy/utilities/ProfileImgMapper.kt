f50b0079c75855922711b9a94e6bddd064bf8e52
Jeong-heon2 <cjhgg2@gmail.com>
2020-08-15 17:12:57 +0900

    프로필 이미지 (#51)

---
+++ app/src/main/java/appvian/water/buddy/utilities/ProfileImgMapper.kt
@@ -0,0 +1,11 @@
+package appvian.water.buddy.utilities
+
+import appvian.water.buddy.R
+
+object ProfileImgMapper {
+    val profile_imgs = arrayListOf(
+        R.drawable.profile_img_1, R.drawable.profile_img_2,
+        R.drawable.profile_img_3, R.drawable.profile_img_4,
+        R.drawable.profile_img_5
+    )
+}
