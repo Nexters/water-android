b0a5095830732b51acb3e04e88776b352e0016db
Jeong-heon2 <cjhgg2@gmail.com>
2020-08-23 10:55:21 +0900

    알림 이미지

--- app/src/main/java/appvian/water/buddy/utilities/AlarmReceiver.kt
+++ app/src/main/java/appvian/water/buddy/utilities/AlarmReceiver.kt
@@ -161,6 +161,7 @@ class AlarmReceiver : BroadcastReceiver() {
         //OREO API 26 이상에서는 채널 필요
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             builder.setSmallIcon(R.drawable.ic_drink_1) //mipmap 사용시 Oreo 이상에서 시스템 UI 에러남
+            builder.setLargeIcon(context.resources.getDrawable(R.drawable.google_play_store_icon, null).toBitmap())
             val channelName = "워비의 수분섭취 알"
             val description = "설정한 시간에 알람합니다."
             val importance = NotificationManager.IMPORTANCE_HIGH //소리와 알림메시지를 같이 보여줌
